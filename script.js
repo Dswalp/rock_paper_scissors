0e3aec0bee2c396bf1242f691c8504f30683fa9b
Dswalp <145557504+Dswalp@users.noreply.github.com>
2024-10-13 17:01:23 +0100

    test

--- script.js
+++ script.js
@@ -23,6 +23,21 @@ const choice = options[Math.floor(Math.random() * options.length)];
 return choice;
 }
 
+function getHumanChoice(){
+    let validatedInput = false;
+    while(validatedInput == false){
+        const choice = prompt("Rock Paper Scissors");
+        if(choice == null) {
+            continue;
+        }
+        const choiceInLower = choice.toLowerCase();
+        if(options.includes(choiceInLower)){
+            validatedInput = true;
+            return choiceInLower;
+        }
+    }
+    }
+
 // Step 4 check winner
 //This function determines the winner based on the player's and computer's selections.
 //If both selections are the same, it returns "Tie".
@@ -66,20 +81,7 @@ function playRound(playerSelection, computerSelection){
 }
 
 }
-function getHumanChoice(){
-    let validatedInput = false;
-    while(validatedInput == false){
-        const choice = prompt("Rock Paper Scissors");
-        if(choice == null) {
-            continue;
-        }
-        const choiceInLower = choice.toLowerCase();
-        if(options.includes(choiceInLower)){
-            validatedInput = true;
-            return choiceInLower;
-        }
-    }
-    }
+
 function game(){
     console.log ("welcome!")
     for (let i = 0; i < 5; i++){
