882dfd94c89ab620564799262e5b7960b1bdd37d
Dswalp <145557504+Dswalp@users.noreply.github.com>
2024-10-13 16:28:39 +0100

    test

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ function playRound(playerSelection, computerSelection){
 
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+console.log(playRound(playerSelection,computerSelection));
 
 
 
