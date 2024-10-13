52824e23a466a2b6381d8cf7a51707d567504341
Dswalp <145557504+Dswalp@users.noreply.github.com>
2024-10-13 15:36:13 +0100

    test

--- script.js
+++ script.js
@@ -13,7 +13,40 @@ function getComputerChoice(){
 const choice = options[Math.floor(Math.random() * options.length)];
 return choice;
 }
+
+function checkWinner(playerSelection, computerSelection){
+    if (playerSelection == computerSelection) {
+        return "Tie";
+    }
+    else if (
+        (playerSelection == "rock" && computerSelection == "scissors")||
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "rock")
+    ) {
+        return "Player";
+    }
+    else {
+        return "Computer";
+    }
+}
+function playRound(playerSelection, computerSelection){
+    const result =checkWinner(playerSelection, computerSelection);
+    if(result == "Tie"){
+        return "It's a tie!"
+    }
+    else if(result =="Player"){
+        return `you win ${playerSelection} beats ${computerSelection}`
+    }
+    else {return `you lose ${computerSelection} beats ${playerSelection}`;
+}
+
+}
+
 getComputerChoice();
+
+
+
+
 
 //get human choice
 
