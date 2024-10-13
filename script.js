43a3240888b552f7b7a39541bd1b2965e3fe455f
Dswalp <145557504+Dswalp@users.noreply.github.com>
2024-10-13 12:20:18 +0100

    test

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //get computer choice function
 
-
+const choice = ["rock", "paper", "scissors"];
 
 function getComputerChoice(choice){
-    const choice = ["rock", "paper", "scissors"];
+   
 
 const random = choice[Math.floor(Math.random()*choice)];
 return random;
